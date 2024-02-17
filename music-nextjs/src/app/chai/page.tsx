7fe365806f1ec36abcefed831c3a7c824bf69a10
Amit Biswas <amit.142biswas@gmail.com>
2024-02-17 23:35:41 +0530

    aceternity ui components added

--- music-nextjs/src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Chaipage(){
-    return(
-        <h1>Chai Page</h1>
-    )
-}
---
+++ music-nextjs/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+
+function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
