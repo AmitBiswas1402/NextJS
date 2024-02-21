a4105ae4302a215e05189d84dcacb924db7e24ca
Amit Biswas <amit.142biswas@gmail.com>
2024-02-21 23:24:09 +0530

    HeroSection Initialized

---
+++ music-nextjs/src/components/HeroSection.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div
+    className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
+    >
+        <div
+        className='p-4 relative z-10 w-full text-center'
+        >
+            <h1>Master the art of Music</h1>
+            <p>Dive into our comprehensive music courses and transform your musical</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
