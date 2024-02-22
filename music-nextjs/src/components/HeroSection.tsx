d06bee6a95310a2b03369a2ffc345d3ebcad1583
Amit Biswas <amit.142biswas@gmail.com>
2024-02-22 20:10:02 +0530

    HeroSection challenges completed

--- music-nextjs/src/components/HeroSection.tsx
+++ music-nextjs/src/components/HeroSection.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
     <div
     className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
     >
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+        />
         <div
         className='p-4 relative z-10 w-full text-center'
         >
@@ -17,7 +23,12 @@ function HeroSection() {
             >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginer or looking to refine your skills, join us to unlocak your true potential.</p>
             <div className="mt-4">
               <Link href={'/courses'}>
-                Explore Courses                
+                  <Button
+                  borderRadius='1.75rem'
+                  className='bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800'
+                  >
+                    Explore Courses
+                  </Button>
               </Link>
             </div>
         </div>
