160ddaa952a1698a98c189b977b4d58ac8e0aa61
Amit Biswas <amit.142biswas@gmail.com>
2024-02-22 10:12:23 +0530

    tailwindcss

--- music-nextjs/src/components/HeroSection.tsx
+++ music-nextjs/src/components/HeroSection.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 function HeroSection() {
@@ -8,8 +9,17 @@ function HeroSection() {
         <div
         className='p-4 relative z-10 w-full text-center'
         >
-            <h1>Master the art of Music</h1>
-            <p>Dive into our comprehensive music courses and transform your musical</p>
+            <h1
+            className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            >Master the art of Music</h1>
+            <p
+            className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginer or looking to refine your skills, join us to unlocak your true potential.</p>
+            <div className="mt-4">
+              <Link href={'/courses'}>
+                Explore Courses                
+              </Link>
+            </div>
         </div>
     </div>
   )
